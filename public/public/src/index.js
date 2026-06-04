81be0b24084dc62bcaf652a0334c466ea1f144aa
El Fantasma <118523056+Orion-thethird@users.noreply.github.com>
2026-06-05 00:08:23 +0100

    Create index.js

---
+++ public/public/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BirthdayWishlist from '../birthday-wishlist';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BirthdayWishlist />
+  </React.StrictMode>
+);
